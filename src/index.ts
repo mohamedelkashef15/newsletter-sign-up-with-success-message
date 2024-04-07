77c55e972e53a143c90ce032ce451ba2ff72a2db
mohamedelkashef15 <mohamedelkashef38@gmail.com>
2024-04-07 22:34:59 +0200

    Add html code

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/*
+  Required Task
+  1- check if email is valid or not 
+  2- if mail is not valid then show error message "Email is not valid"
+  3- if mail is valid then show success message 
+*/
